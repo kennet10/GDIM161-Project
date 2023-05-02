e4020f1845aacaf91c1588852642a65a515a2f77
DarkSoulsV <92570188+DarkSoulsV@users.noreply.github.com>
2023-05-01 22:06:25 -0700

    1

--- GDIM161 Project/Assets/Scripts/SoundTrigger.cs
+++ GDIM161 Project/Assets/Scripts/SoundTrigger.cs
@@ -6,7 +6,7 @@ public class SoundTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Ball"))
         {
             audioSource.Play();
         }
