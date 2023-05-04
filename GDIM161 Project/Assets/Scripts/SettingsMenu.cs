f2d17411d7c0db6ef37469aece093365755da548
DarkSoulsV <92570188+DarkSoulsV@users.noreply.github.com>
2023-05-03 23:45:24 -0700

    1

--- GDIM161 Project/Assets/Scripts/SettingsMenu.cs
+++ GDIM161 Project/Assets/Scripts/SettingsMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -14,7 +15,13 @@ public class SettingsMenu : MonoBehaviour
 
     private void Start()
     {
-        resolutions = Screen.resolutions;
+        // Set default resolution, aspect ratio, and full-screen mode
+        Resolution defaultResolution = new Resolution { width = 3840, height = 2160 };
+        float defaultAspectRatio = 16f / 9f;
+        bool defaultFullScreen = true;
+
+        // Set resolution dropdown options
+        resolutions = Screen.resolutions.Where(resolution => Mathf.Approximately((float)resolution.width / resolution.height, defaultAspectRatio)).ToArray();
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
         int currentResIndex = 0;
@@ -23,7 +30,7 @@ public class SettingsMenu : MonoBehaviour
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
 
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            if (resolutions[i].width == defaultResolution.width && resolutions[i].height == defaultResolution.height)
             {
                 currentResIndex = i;
             }
@@ -32,6 +39,10 @@ public class SettingsMenu : MonoBehaviour
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResIndex;
         resolutionDropdown.RefreshShownValue();
+
+        // Set initial resolution and full-screen mode
+        Screen.SetResolution(defaultResolution.width, defaultResolution.height, defaultFullScreen);
+        Screen.fullScreen = defaultFullScreen;
     }
 
     /*public void SetVolume(float volume)
